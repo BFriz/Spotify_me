c4c8e6efa75dbc777bbfa1cde933fb2fa2d1f02e
Ben Frisby <bfrisb@gmail.com>
2015-05-28 20:55:30 +0100

    this sucks

--- spotify-me-homework 2/js/spotify.js
+++ spotify-me-homework 2/js/spotify.js
@@ -1,29 +1,47 @@
-// // https://api.spotify.com/v1/search?q=SOMETHINGHERE&type=SOMETHINGHERE
-// $(document).ready(function() {
+// // // https://api.spotify.com/v1/search?q=SOMETHINGHERE&type=SOMETHINGHERE
+// // $(document).ready(function() {
 
 
-// 
-// });
-$('#submit').on('click', function(){
-event.preventDefault();
-var user = [];
+// // 
+// // });
+// $('#submit').on('click', function(){
+// event.preventDefault();
+// dropdownPick = $('#search-type').val();
+// userInput = $('#search-keyword').val()
+//  $.ajax({
+//    type: "GET",
+//    url: 'https://api.spotify.com/v1/search?q='+userInput+'&type=' + dropdownPick
+//  }).done(function(data, response){
+//    // console.log(userInput);
+//    $.each(data.albums.items, function(index, item){
+//     console.log(item);
+//     // if($.inArray(item, userInput) === -1) {
+//       $('#results').append("<li>"+ item.name + "</li>");
 
-var userInput = $('#search-keyword').val()
- $.ajax({
-   type: "GET",
-   url: 'https://api.spotify.com/v1/search?q='+userInput+'&type=artist'
- }).done(function(response){
-   console.log(userInput);
-   var inputSelect = $('#search-keyword')
-   inputSelect.empty();
-   $.each(response, function(index, item){
-    console.log(item);
-    if($.inArray(item, userInput) === -1) {
-      user.push(item.region);
-      inputSelect.append('<body><iframe src="https://api.spotify.com/v1/search?q='+userInput+'&type=artist"width="300" height="380" frameborder="0" allowtransparency="true"></iframe></body>');
-    }
-// <iframe src="https://embed.spotify.com/?uri=spotify:track:7B9S4RiRczdw1B9ddjpFe8" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
-    // if($)
-   })
- })
-})
+//       // $('results').append("<li>" + item.name +"</li>")
+//     // }
+//    })
+//  })
+// })
+
+// https://api.spotify.com/v1/search?q=SOMETHINGHERE&type=SOMETHINGHERE
+$(document).ready(function() {
+
+  $('#submit_button').on('click', function(event) {
+    event.preventDefault();
+    search = $('#search-keyword').val();
+    console.log(search);
+    dropdownPick = $('#search-type').val();
+    console.log(dropdownPick);
+
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.spotify.com/v1/search?q=' + search +'&type=' + dropdownPick
+    }).done(function(data, response) {
+      $.each(data.albums.items, function (index, item) {
+        console.log(item);
+      $('#results').append("<li>" + item.name + "</li>")
+    })
+    })
+  })
+});
