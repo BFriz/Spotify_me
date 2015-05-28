e3bc2170df4dce58c8e3ea56d0b52f274928d9f7
Ben Frisby <bfrisb@gmail.com>
2015-05-28 18:31:03 +0100

    haven't done anything really just thought I should commit

--- spotify-me-homework 2/js/spotify.js
+++ spotify-me-homework 2/js/spotify.js
@@ -1,4 +1,27 @@
 // https://api.spotify.com/v1/search?q=SOMETHINGHERE&type=SOMETHINGHERE
-$(document).ready(function() {
-  
+// $(document).ready(function() {
+// });
+
+$('#submit').on('click', function(){
+  searching();
 });
+
+function searching () {
+  var artists = [];
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.spotify.com/v1/search?q='+input+'&type='+searchType
+  }).done(function(response){
+//     info = $('#search-keyword').val()
+// console.log(this)
+// console.log(artists)
+// console.log(searchType)
+//   })
+
+
+// }
+
+
+// var input = $('#search-keyword').val()
+// var searchType = $('#search-type').val()
